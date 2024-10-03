cf8d751fb2dd41a5ca1e7a0610dd59ebde4db5d6
BuiPhuoc <146042853+buiphuocc@users.noreply.github.com>
2024-10-03 18:36:01 +0700

    change connection string in dbcontext to postgre

--- Repositories/Data/MealHuntContext.cs
+++ Repositories/Data/MealHuntContext.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using MealHunt_Repositories.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace MealHunt_Repositories.Data;
 
@@ -42,8 +43,15 @@ public partial class MealHuntContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=localhost;Database=MealHunt;User ID=sa;Password=12345;Trust Server Certificate=True");
-
+        => optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Username=postgres;Password=12345;Database=mealhuntdb;");
+    private string GetConnectionString()
+    {
+        IConfiguration configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", true, true).Build();
+        var connection = configuration.GetConnectionString("MealHuntDB");
+        return connection;
+    }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Category>(entity =>
