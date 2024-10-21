65cb9c4d1a4fa0358fec6b7c5c2cf139c4e655db
Khoa Nguyễn <khoa.nguyen090703@gmail.com>
2024-10-21 21:30:47 +0700

    refactor: Database (Add payment entity)

---
+++ Repositories/Entities/Payment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MealHunt_Repositories.Entities
+{
+    public class Payment
+    {
+        public string Id { get; set; }
+
+        public double Amount { get; set; }
+
+        public DateTime PayDate { get; set; }
+
+        public string TransactionId {  get; set; }
+        
+        public int? UserId {  get; set; }
+        
+        public virtual User? User { get; set; }
+    }
+}
